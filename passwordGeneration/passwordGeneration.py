e37b1ff58b9040a7bc3f2fce1f4ba39ec730a6c0
Alan Dev <101974432+All4nBK@users.noreply.github.com>
2023-04-19 20:55:57 -0300

    Default

--- passwordGeneration/passwordGeneration.py
+++ passwordGeneration/passwordGeneration.py
@@ -1,30 +1,11 @@
 #Importação da biblioteca Random;
 import random
-#Importação da biblioteca OS;
-import os
-
-#Identificar as pastas do Usuario
-usarname = os.environ
-try:
-  os.makedirs(os.path.join(r'C:\Users',usarname['USERNAME'],r'Documents\Password'))
-  try:
-    open(os.path.join(folder,"password.txt"), 'x')
-  except:
-    print('Atualizado')
-  print('criado')
-except:
-  print('')
-folder = os.path.join(r'C:\Users',usarname['USERNAME'],'Documents\Password')
-
-# Abrir o arquivo em modo de leitura para ler o conteúdo original
-with open(os.path.join(folder,"password.txt"), 'r') as arquivo:
-    conteudo_original = arquivo.read()
 
 #Informações dos caracteres que seram permitidos para a criação de senhas;
 letras_maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 letras_minusculas = "abcdefghijklmnopqrstuvwxyz"
 numeros = "0123456789"
-simbolos = "!@#$%&*()_+=[]{}|;:,.<>/?"
+simbolos = "!@#$%&*()_+-=[]{}|;:,.<>/?"
 todos_caracteres = letras_maiusculas + letras_minusculas + numeros + simbolos
 
 #Agora irei definir uma variavel do comprimento da senha;
@@ -38,13 +19,7 @@ def generationPassword(a,b):
   return senha
 
 #Agora é vamos fazer uma forma de mostrar a senha na tela;
-info = input('Para qual software você quer salvar essa senha: ')
 senha = generationPassword(todos_caracteres,comprimento_senha)
 print("Sua senha é: ",senha)
-
-with open(os.path.join(folder,"password.txt"), 'a') as arquivo:
-  arquivo.write(senha+'- Senha salva para software - '+info+'\n')
-  
-print('Criado com Sucesso👌\nAtualizado')
   
   
